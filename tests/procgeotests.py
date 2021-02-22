e059f7c4ccadc508d78f1c1b7275dd99dcd3bdbb
filipe.silvestrim <filipe.silvestrim@innogames.com>
2021-02-22 23:02:25 +0100

    fixed sqaure unit and bounds

--- tests/procgeotests.py
+++ tests/procgeotests.py
@@ -3,6 +3,7 @@ import unittest
 
 from tests.test_utils import repeat
 from utils.procgeo import ProcGeo
+from parameterized import parameterized
 
 
 class AProcGeoTest(metaclass=abc.ABCMeta):
@@ -16,19 +17,19 @@ class AProcGeoTest(metaclass=abc.ABCMeta):
         self.cud = ProcGeo(*settings)
 
     @repeat(100)
-    def test_get_random_line_should_work_for_same_angle(self):
+    def test__get_random_line__should_work_for_same_angle(self):
         self.cud.get_random_line(1, 1, False)
 
     @repeat(100)
-    def test_get_random_line_should_work_for_0_90_angle(self):
+    def test__get_random_line__should_work_for_0_90_angle(self):
         self.cud.get_random_line(0, 90, False)
 
     @repeat(100)
-    def test_get_random_line_should_work_for_0_180_angle(self):
+    def test__get_random_line__should_work_for_0_180_angle(self):
         self.cud.get_random_line(0, 180, False)
 
     @repeat(100)
-    def test_get_random_line_should_work_for_0_360_angle(self):
+    def test__get_random_line__should_work_for_0_360_angle(self):
         self.cud.get_random_line(0, 360, False)
 
 
@@ -42,6 +43,28 @@ class ProcGeoTest28(AProcGeoTest, unittest.TestCase):
 
     def get_settings(self):
         return (28, 28), 2, 0
+
+
+class ProcGeoTestUtils(unittest.TestCase):
+
+    @parameterized.expand([
+        [0,     0,  200, 0,     200],  # 0
+        [2,     0,  200, 0,     198],  # 1
+        [2,     2,  198, 2,     196],  # 2
+        [20,    0,  200, 0,     180],  # 3
+        [180,   0,  200, 0,     20],   # 4
+        [180,   1,  198, 1,     18],   # 5
+        [100,   0,  200, 0,     100],  # 6
+        [-20,   0,  200, 20,    200],  # 7
+        [-20,   2,  198, 22,    198],  # 8
+        [-180,  0,  200, 180,   200],  # 9
+        [497,   1,  499, 1,     2],    # 10
+        [2,     1,  9,   1,     7],    # 11
+    ])
+    def test__get_min_max_bounds(self, val, minimum, maximum, expected_min, expected_max):
+        boundaries = ProcGeo.get_min_max_bounds(val, minimum, maximum)
+        self.assertEqual(expected_min, boundaries[0], "MIN - val:%i, min:%i, max:%i" % (val, minimum, maximum))
+        self.assertEqual(expected_max, boundaries[1], "MAX - val:%i, min:%i, max:%i" % (val, minimum, maximum))
 
 
 if __name__ == '__main__':
--- utils/procgeo.py
+++ utils/procgeo.py
@@ -37,41 +37,56 @@ class ProcGeo:
 
         return box
 
-    def __get_min_bounds(self, length, minimum):
-        return minimum if length > minimum else -length
+    @staticmethod
+    def get_min_max_bounds(val, minimum, maximum):
+        if val > maximum:
+            raise ValueError("Value %i is out of bounds of %i" % (val, maximum))
 
-    def __get_max_bounds(self, length, maximum):
-        return maximum - length if length < maximum else maximum - length
+        low = minimum if val >= minimum else -val + minimum
+        high = maximum - val if val >= minimum else maximum
 
-    def __get_min_max_bounds(self, val0, val1, min_size, max_size):
-        bounds_min = np.maximum(self.__get_min_bounds(val0, min_size), self.__get_min_bounds(val1, min_size))
-        bounds_max = np.maximum(self.__get_max_bounds(val0, max_size), self.__get_max_bounds(val1, max_size))
+        if low >= high:
+            raise ValueError("low (%i) is bigger than high (%i). val:%i, min:%i, max:%i" % (low, high, val, minimum, maximum))
 
-        return bounds_min, bounds_max
+        return low, high
+
+    #
+    # @staticmethod
+    # def get_min_max_bounds(self, val0, val1, min_size, max_size):
+    #     bounds_min = np.maximum(self.get_min_bounds(val0, min_size), self.get_min_bounds(val1, min_size))
+    #     bounds_max = np.maximum(self.get_max_bounds(val0, max_size), self.get_max_bounds(val1, max_size))
+    #
+    #     return bounds_min, bounds_max
 
     def get_random_line(self, start_angle_range_degree, end_angle_range_degree, as_array=True):
+        # get random angle
         angle_degree = np.random.randint(start_angle_range_degree, high=end_angle_range_degree) \
             if start_angle_range_degree != end_angle_range_degree else start_angle_range_degree
         angle_radians = np.radians(angle_degree)
 
-        tan_projection = np.minimum(np.abs(np.tan(angle_radians)), 1)
-        max_length_x = tan_projection * (self.width - self.margin_safe_area)
-        cotan_projection = np.minimum(np.abs(1 / np.tan(angle_radians)), 1)
-        max_length_y = cotan_projection * (self.height - self.margin_safe_area)
+        # find projections on the square unit
+        x_projection = np.clip(np.abs(1/np.tan(np.radians(angle_radians))), 0, 1)
+        y_projection = np.clip(np.abs(np.tan(np.radians(angle_radians))), 0, 1)
+
+        # now we know the longest length
+        max_length_x = x_projection * (self.width - self.margin_safe_area*2)
+        max_length_y = y_projection * (self.height - self.margin_safe_area*2)
         max_length = np.hypot(max_length_x, max_length_y)
 
+        # get random length
         length = np.random.randint(self.min_pts_distance, high=max_length)
 
+        # find the new 2d pt from the origin
         pt1 = np.array([np.cos(angle_radians) * length, np.sin(angle_radians) * length])
 
-        pt0_x_min_max = self.__get_min_bounds(pt1[0], 0), \
-                        self.__get_max_bounds(pt1[0], self.width - self.margin_safe_area)
+        pt0_x_min_max = self.get_min_max_bounds(pt1[0], self.margin_safe_area, self.width - self.margin_safe_area)
+        pt0_y_min_max = self.get_min_max_bounds(pt1[1], self.margin_safe_area, self.height - self.margin_safe_area)
 
-        pt0_y_min_max = self.__get_min_bounds(pt1[1], 0), \
-                        self.__get_max_bounds(pt1[1], self.height - self.margin_safe_area)
-
-        pt0 = np.array([np.random.randint(pt0_x_min_max[0], high=pt0_x_min_max[1]), \
-                        np.random.randint(pt0_y_min_max[0], high=pt0_y_min_max[1])])
+        pt0 = np.array([
+            np.random.randint(pt0_x_min_max[0], high=pt0_x_min_max[1])
+            if pt0_x_min_max[0] != pt0_x_min_max[1] else pt0_x_min_max[0],
+            np.random.randint(pt0_y_min_max[0], high=pt0_y_min_max[1])
+            if pt0_y_min_max[0] != pt0_y_min_max[1] else pt0_y_min_max[0]])
 
         pt0 = np.int0(pt0)
         pt1 = np.int0(pt0 + pt1)
@@ -88,16 +103,16 @@ class ProcGeo:
         start_angle = np.radians(np.random.randint(start_angle_degree, high=end_angle_degree))
         end_angle = np.radians(np.random.randint(start_angle_degree + min_angle_degree, high=end_angle_degree))
 
-        length = np.random.randint(self.min_pts_distance, high=self.max_pts_distance)
+        length = np.random.randint(self.min_pts_distance, high=self.margin_safe_area)
 
         pt0 = np.array([np.cos(start_angle) * length, np.sin(start_angle) * length])
         pt1 = np.array([np.cos(end_angle) * length, np.sin(end_angle) * length])
 
-        center_x_min_max = self.__get_min_max_bounds(pt0[0], pt1[0], self.min_pts_distance,
-                                                     self.width - self.margin_safe_area)
+        center_x_min_max = self.get_min_max_bounds(pt0[0], pt1[0], self.min_pts_distance,
+                                                   self.width - self.margin_safe_area)
 
-        center_y_min_max = self.__get_min_max_bounds(pt0[1], pt1[1], self.min_pts_distance,
-                                                     self.height - self.margin_safe_area)
+        center_y_min_max = self.get_min_max_bounds(pt0[1], pt1[1], self.min_pts_distance,
+                                                   self.height - self.margin_safe_area)
 
         center = np.array([np.random.randint(center_x_min_max[0], high=center_x_min_max[1]), \
                            np.random.randint(center_y_min_max[0], high=center_y_min_max[1])])
